e116c979ce75ba6fab7488b3e59389ab983df78b
CyberCode2012 <142232635+CyberCode2012@users.noreply.github.com>
2023-08-25 16:52:07 +0400

    Brush preview and BG color
    Added brush preview with preview of size, shape, color. Also BG color was added. You can now change BG color simply

--- simple_paint_app.py
+++ simple_paint_app.py
@@ -5,10 +5,20 @@ from PIL import ImageGrab, Image, ImageTk
 
 class PaintApp:
     def __init__(self, root):
+        self.brush_size = 10  # A more noticeable size
+        self.shape = "Oval"   # A more noticeable shape
+        self.color = "black"
         self.root = root
         self.root.title("BrushMagic: Your Digital Canvas")
 
         self.canvas = tk.Canvas(root, bg="white", width=800, height=600, highlightthickness=0)
+        self.canvas.grid(row=0, column=0, columnspan=4, rowspan=3, sticky="nsew")
+
+
+        root.grid_rowconfigure(0, weight=1)
+        root.grid_columnconfigure(0, weight=1)
+
+        self.canvas = tk.Canvas(root, bg="white", highlightthickness=0)
         self.canvas.grid(row=0, column=0, columnspan=4, rowspan=3, sticky="nsew")
 
         self.brush_size = 2
@@ -18,6 +28,8 @@ class PaintApp:
         self.color = "black"
         self.last_selected_color = self.color
         self.shape = "line"  # Default shape is line
+        self.undo_stack = []
+        self.redo_stack = []
 
         self.canvas.bind("<Button-1>", self.start_draw)
         self.canvas.bind("<B1-Motion>", self.draw)
@@ -26,6 +38,10 @@ class PaintApp:
         # Brush size control and shape changer at the bottom
         self.bottom_frame = tk.Frame(root)
         self.bottom_frame.grid(row=3, column=0, columnspan=4, pady=10, sticky="nsew")
+
+        self.brush_preview = tk.Canvas(self.bottom_frame, width=50, height=50, bg="white")
+        self.brush_preview.pack(side=tk.LEFT, padx=5)
+        self.update_brush_preview()
 
         self.brush_size_label = tk.Label(self.bottom_frame, text="Brush Size:")
         self.brush_size_label.pack(side=tk.LEFT)
@@ -60,6 +76,12 @@ class PaintApp:
         self.load_button = tk.Button(self.bottom_frame, text="Load", command=self.load_image)
         self.load_button.pack(side=tk.LEFT, padx=5)
 
+        self.bg_color_button = tk.Button(self.bottom_frame, text="BG Color", command=self.change_bg_color)
+        self.bg_color_button.pack(side=tk.LEFT, padx=5)
+
+        self.update_brush_preview()
+
+
     def start_draw(self, event):
         self.last_x = event.x
         self.last_y = event.y
@@ -82,17 +104,20 @@ class PaintApp:
     def stop_draw(self, event):
         self.draw_status = False
 
+
     def clear_canvas(self):
         self.canvas.delete("all")
 
     def change_brush_size(self, new_size):
         self.brush_size = int(new_size)
+        self.update_brush_preview()
 
     def change_color(self):
         color = colorchooser.askcolor(title="Choose a color")
         if color[1]:
             self.color = color[1]
             self.last_selected_color = self.color
+            self.update_brush_preview()
 
     def use_eraser(self):
         self.color = "white"
@@ -102,6 +127,7 @@ class PaintApp:
 
     def change_shape(self, selected_shape):
         self.shape = selected_shape
+        self.update_brush_preview()
 
     def save_canvas(self):
         file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
@@ -120,6 +146,48 @@ class PaintApp:
             image = image.resize((800, 600), Image.ANTIALIAS)
             self.image_tk = ImageTk.PhotoImage(image)
             self.canvas.create_image(0, 0, anchor=tk.NW, image=self.image_tk)
+    
+    def change_bg_color(self):
+        color = colorchooser.askcolor(title="Choose a background color")
+        if color[1]:
+            self.canvas.config(bg=color[1])
+
+    def clear_canvas(self):
+        current_bg = self.canvas.cget("bg")
+        self.canvas.delete("all")
+        self.canvas.config(bg=current_bg)
+
+    def update_brush_preview(self):
+        self.brush_preview.delete("all")
+        x, y = 25, 25  # Center of the canvas
+
+        if self.shape == "Line":
+            self.brush_preview.create_oval(x - self.brush_size/2, y - self.brush_size/2, x + self.brush_size/2, y + self.brush_size/2, fill=self.color)
+        elif self.shape == "Rectangle":
+            size = self.brush_size * 2
+            half_size = size / 2
+            self.brush_preview.create_rectangle(x - half_size, y - half_size, x + half_size, y + half_size, outline=self.color, width=self.brush_size)
+        elif self.shape == "Oval":
+            size = self.brush_size * 2
+            half_size = size / 2
+            self.brush_preview.create_oval(x - half_size, y - half_size, x + half_size, y + half_size, outline=self.color, width=self.brush_size)
+        else:
+            self.brush_preview.create_oval(x - self.brush_size/2, y - self.brush_size/2, x + self.brush_size/2, y + self.brush_size/2, fill=self.color)
+
+    def undo(self):
+        if self.undo_stack:
+            last_image = self.undo_stack.pop()
+            self.redo_stack.append(self.canvas.copy())
+            self.canvas.delete("all")
+            self.canvas.create_image(0, 0, image=last_image, anchor=tk.NW)
+
+    def redo(self):
+        if self.redo_stack:
+            last_image = self.redo_stack.pop()
+            self.undo_stack.append(self.canvas.copy())
+            self.canvas.delete("all")
+            self.canvas.create_image(0, 0, image=last_image, anchor=tk.NW)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
